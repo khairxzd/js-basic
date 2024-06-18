2bc8e68ec669ef11add3ad4e87bd2fbdf3eb1251
T91 GAMING CENTER <khairxzd@gmail.com>
2024-06-19 04:09:44 +0700

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+const myArr = ["html", "css"];
+
+console.log(myArr);
+/**
+ * - Variables
+ *
+ * - Comments
+ *
+ * - Built-in functions: alert, console, confirm, prompt, set timeout, set interval
+ *
+ * - Operators: arithmetic, assignment, comparison, logical
+ *
+ * - Data types: number, string, boolean, undefined, null, symbol, function, object
+ *
+ * - Truthy and falsy
+ * - Falsy values: 0, "", '', NaN, null, undefined
+ */
