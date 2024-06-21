627f7c9cc337be6aa6e82049bdfb86c758716fa3
Khairxzd Kieu <khairxzd@gmail.com>
2024-06-21 12:41:56 +0700

    up to code

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 function sum(a, b) {
   console.log(a + b);
 }
+
+document.write("Tao Button va Text bang Script<BR>");
+document.write("<BR>");
+document.write("<input type=button name=welcome value = 'Welcome' ");
+document.write("onclick = 'alert ('Welcome to JavaScript');' > ");
+document.write("<input type = text name = msg value = 'Welcome to'>");
+
+const hide = (...el) => [...el].forEach((e) => (e.style.display = "none"));
+
+// Ví dụ
+// Ẩn tất cả các phần tử <img> trên trang
+hide(document.querySelectorAll("img"));
+
+const hasClass = (el, className) => el.classList.contains(className);
+
+// Ví dụ
+hasClass(document.querySelector("p.special"), "special"); // true
+
+const toggleClass = (el, className) => el.classList.toggle(className);
+
+// Ví dụ
+toggleClass(document.querySelector("p.special"), "special");
+// Thẻ p sẽ không còn class "special" nữa
 /**
  * - Variables
  *
